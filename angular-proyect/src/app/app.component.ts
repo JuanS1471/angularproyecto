523fd5be18867006c29383ffacf732fecd1a90dc
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-07-18 08:27:31 -0500

    Estructura y flujo

--- angular-proyect/src/app/app.component.ts
+++ angular-proyect/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-proyect';
+  title = 'Proyecto de angular';
+  nombre = 'Juan'
 }
