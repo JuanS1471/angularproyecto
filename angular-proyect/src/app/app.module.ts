ca1d1dff26d63d6bc1c7db989cae687688782e7f
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-07-21 08:30:32 -0500

    Componentes II

--- angular-proyect/src/app/app.module.ts
+++ angular-proyect/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './component/componente/componente.component';
+import { ComponentchildComponent } from './componentehijo/componentchild/componentchild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent,
+    ComponentchildComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-proyect/src/app/componentehijo/componentchild/componentchild.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentchildComponent } from './componentchild.component';
+
+describe('ComponentchildComponent', () => {
+  let component: ComponentchildComponent;
+  let fixture: ComponentFixture<ComponentchildComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComponentchildComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComponentchildComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
