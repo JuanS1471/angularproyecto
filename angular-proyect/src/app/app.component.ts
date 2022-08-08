1116e1406dd3256b1b85cc97d219c1016e37dddb
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-08-08 09:00:16 -0500

    Directivas 5

--- angular-proyect/src/app/app.component.ts
+++ angular-proyect/src/app/app.component.ts
@@ -8,12 +8,15 @@ import { empleado } from './empleado.model';
 })
 export class AppComponent {
   titulo='Listado empleados';
-
+  
   empleados:empleado[]=[
     new empleado("Jose","gomez","Developer",500000),
     new empleado("Andres","lopez","Backend",600000),
     new empleado("Martin","serna","SQL",700000),
     new empleado("Adriana","gomez","Directivo",800000)
   ]
-
+  nombreVar:string = "";
+  apellidoVar:string = "";
+  cargoVar:string = "";
+  salarioVar:number = 0;
 }
