e0eb7dffe760acdb87d9f1509d145666ead6dc89
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-08-10 08:30:28 -0500

    ngStyle

--- angular-proyect/src/app/app.component.ts
+++ angular-proyect/src/app/app.component.ts
@@ -10,10 +10,6 @@ export class AppComponent {
   titulo='Listado empleados';
   
   empleados:empleado[]=[
-    new empleado("Jose","gomez","Developer",500000),
-    new empleado("Andres","lopez","Backend",600000),
-    new empleado("Martin","serna","SQL",700000),
-    new empleado("Adriana","gomez","Directivo",800000)
   ]
   nombreVar:string = "";
   apellidoVar:string = "";
