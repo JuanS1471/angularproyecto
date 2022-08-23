6b5d5c9957f777969a4d63731f3c3b1f6c799f6a
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-08-23 08:26:30 -0500

    Routing 2

--- angular-proyect/src/app/home-component/home-component.component.ts
+++ angular-proyect/src/app/home-component/home-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { empleado } from '../empleado.model';
+import { empleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-home-component',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
+  titulo='Listado empleados';
+  constructor(
+   private notification: ServicioEmpleadosService,
+   private empleadoService: empleadosService
+  ){
 
-  constructor() { }
-
+  }
   ngOnInit(): void {
   }
-
+  empleados = this.empleadoService.empleados
+  nombreVar:string = "";
+  apellidoVar:string = "";
+  cargoVar:string = "";
+  salarioVar:number = 0;
+  agregarEmp(){
+    let miempleado = new empleado(this.nombreVar, this.apellidoVar, this.cargoVar, this.salarioVar);
+    this.empleadoService.addUser(miempleado);
+  }
 }
