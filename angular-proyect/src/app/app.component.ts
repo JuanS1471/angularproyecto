6549715321cb2518c4f042e77ee59fd837dcb82a
Hayesito <juaeserna1@misena.edu.co>
2022-09-14 08:32:40 -0500

    login 3

--- angular-proyect/src/app/app.component.ts
+++ angular-proyect/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { empleado } from './empleado.model';
 import { empleadosService } from './empleados.service';
 import { ServicioEmpleadosService } from './servicio-empleados.service';
-
+import firebase  from 'firebase/compat/app'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titulo='Listado empleados';
   constructor(
    private notification: ServicioEmpleadosService,
@@ -16,6 +16,13 @@ export class AppComponent {
   ){
 
   }
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCfQTVuHgLAEeRPCDoxbgrRbueuxlqespg",
+      authDomain: "proyectangular-91409.firebaseapp.com",
+    })
+  }
+
   empleados = this.empleadoService.empleados
   nombreVar:string = "";
   apellidoVar:string = "";
