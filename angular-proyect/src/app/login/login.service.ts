68c1279c360baab1ca88304370c9f5c6b7d4786a
Hayesito <juaeserna1@misena.edu.co>
2022-09-15 08:18:44 -0500

    Login 4

---
+++ angular-proyect/src/app/login/login.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import firebase  from "firebase/compat/app";
+import 'firebase/compat/auth'
+
+@Injectable()
+export class loginService{
+    constructor(
+        private router:Router
+    ){
+
+    }
+    token!:string
+
+    login(email:string, passw: string){
+        firebase.auth().signInWithEmailAndPassword(email, passw).then(
+            response =>{
+                firebase.auth().currentUser?.getIdToken().then(
+                    token =>{
+                        this.token = token;
+                        this.router.navigate(['/']);
+                    }
+                )
+            }
+        )
+    }
+
+    getidToken(){
+        return this.token;
+    }
+
+}
